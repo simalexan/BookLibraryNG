a8721cc1b46e19244e55b5aefe4edbb4f28126c6
Alexander Simovic <alexander.simovic@gmail.com>
2013-06-23 14:42:17 +0200

    Last class  with some additional changes - added the usage of a service "bookService" - modified the corresponding code - detail now has a list of all books

--- js/app.js
+++ js/app.js
@@ -1,14 +1,43 @@
-var book = {};
-book.id = 1;
-book.name = "Bugarska skupština";
-book.author = "Igor Gašparević";
+var bookLibApp = angular.module('bookLib', []).
+    config(function($routeProvider) {
+        $routeProvider.
+            when('/', {controller:BookCtrl, templateUrl:'bookDetail.html'}).
+            when('/new', {controller:BookCtrl, templateUrl:'addBook.html'}).
+            otherwise({redirectTo:'/'});
+    });
 
-function fillUpData(){
-    console.log("called");
-    console.log("checking book id " + book.id);
-    $('#BookID').html(book.id);
-    $('#BookName').html(book.name);
-    $('#BookAuthor').html(book.author);
+function BookCtrl($scope, $location, bookService){
+    console.log("Book controller initalized");
+    $scope.books = bookService.books;
+    console.log(JSON.stringify($scope.books));
+    $scope.book = $scope.books[0];
+    $scope.newBook = {};
+    $scope.addNewBook = function() {
+        // first we validate the book
+        if($scope.newBook.id){
+            // then we push it to the service
+            bookService.books.push($scope.newBook);
+            // then open the index
+            $scope.openPage('/');
+        } else {
+            alert("ERROR: bookIsEmpty");
+        }
+    };
+
+    $scope.openPage = function(newPath) {
+        console.log(JSON.stringify($scope.books));
+        console.log("about to open a new path");
+        $location.path(newPath);
+    };
 }
 
-//fillUpData();
+bookLibApp.factory('bookService', function() {
+    var currentOpenedBook = {};
+    var books = [];
+
+    return {
+        book: currentOpenedBook,
+        books: books
+    };
+});
+
