9b0d0a3f736820f01359281176363912f22b3c96
simalexan <alexander.simovic@gmail.com>
2014-08-17 15:52:16 +0200

    adding promise for adding a book

--- app/scripts/controllers/book-ctrl.js
+++ app/scripts/controllers/book-ctrl.js
@@ -1,13 +1,15 @@
 /**
  * Created by simalexan on 8/17/14.
  */
-bookLibApp.controller('BookCtrl', function ($scope, $location, bookFactory) {
+bookLibApp.controller('BookCtrl', function ($scope, $location, BookFactory) {
 
-  $scope.books = bookFactory.books;
+  $scope.books = BookFactory.books;
   $scope.book = $scope.books[0];
   $scope.newBook = {};
   $scope.addNewBook = function (newBook){
-    bookFactory.addNewBook(newBook);
+    BookFactory.addNewBook(newBook).then(function (){
+      $scope.openPage('/');
+    });
   };
 
   $scope.openPage = function (newPath) {
--- app/scripts/services/book-factory.js
+++ app/scripts/services/book-factory.js
@@ -1,22 +1,26 @@
 /**
  * Created by simalexan on 8/17/14.
  */
-bookLibApp.factory('bookFactory', function () {
-  var currentOpenedBook = {};
+bookLibApp.factory('BookFactory', function ($q) {
+  var currentBook = {};
   var books = [];
   return {
-    book: currentOpenedBook,
+    book: currentBook,
     books: books,
     /**
      * Adding a new book
      * @param {Object} newBook
      */
     addNewBook: function (newBook) {
+      var deferred = $q.defer();
       if (newBook.id) {
         this.books.push(newBook);
+        deferred.resolve();
       } else {
+        deferred.reject();
         throw new Error('The new book you wanted to add is empty');
       }
+      return deferred.promise;
     }
   };
 });
